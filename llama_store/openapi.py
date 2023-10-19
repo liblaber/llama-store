2db23417fce62b01a806d3f356927fb6abc57560
Jim Bennett <jimbobbennett@liblab.com>
2023-10-19 00:17:51 +0000

    Typos and dev container improvements

--- llama_store/openapi.py
+++ llama_store/openapi.py
@@ -69,7 +69,7 @@ def fix_openapi_spec(app: FastAPI) -> None:
     """
     The default OpenAPI spec created by FastAPI has trailing slashes on the server URLs. This function removes them, as this
     is a validation error in the spectral analysis of the OpenAPI spec.
-    See this discussion from the FastAPIO GitHub repo: https://github.com/tiangolo/fastapi/discussions/10309
+    See this discussion from the FastAPI GitHub repo: https://github.com/tiangolo/fastapi/discussions/10309
 
     This also adds bearer auth to the security schemes and the top level security section
     """
