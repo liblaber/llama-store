f0a2864e293141c032d3d2658e46383b5fe88221
Jim Bennett <jimbobbennett@liblab.com>
2024-03-08 01:21:59 +0000

    Adding C# demos

--- hooks/csharp/CustomHook.cs
+++ hooks/csharp/CustomHook.cs
@@ -1,20 +1,22 @@
+#pragma warning disable 1998 // This async method lacks 'await' operators
+#pragma warning disable 8603 // Possible null reference return.
 
 public class CustomHook : IHook
 {
   public async Task<HttpRequestMessage> BeforeRequestAsync(HttpRequestMessage request)
   {
-    Console.WriteLine($"Before request on URL {request.RequestUri.AbsoluteUri} with method {request.Method.ToUpper()}");
+    Console.WriteLine($"Before request on URL {request?.RequestUri?.AbsoluteUri} with method {request?.Method.ToString().ToUpper()}");
     return request;
   }
 
   public async Task<HttpResponseMessage> AfterResponseAsync(HttpResponseMessage response)
   {
-    Console.WriteLine($"After response on URL {response.RequestMessage.RequestUri.AbsoluteUri} with method {response.RequestMessage.Method.ToUpper()}, returning status {response.StatusCode}")
+    Console.WriteLine($"After response on URL {response?.RequestMessage?.RequestUri?.AbsoluteUri} with method {response?.RequestMessage?.Method.ToString().ToUpper()}, returning status {response?.StatusCode}");
     return response;
   }
 
   public async Task OnErrorAsync(HttpResponseMessage response)
   {
-    Console.WriteLine($"On error - {response.StatusCode} - {response.ReasonPhrase}");
+    Console.WriteLine($"On error - {response?.StatusCode} - {response?.ReasonPhrase}");
   }
 }
