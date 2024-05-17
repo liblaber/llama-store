7a9ca15add95c12dcdda308577aea83b841243ab
Jim Bennett <jimbobbennett@liblab.com>
2024-05-17 00:08:38 +0000

    Code tidying

--- docs/src/theme/ApiExplorer/Request/index.tsx
+++ docs/src/theme/ApiExplorer/Request/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import Request from '@theme-original/ApiExplorer/Request';
-import type RequestType from '@theme/ApiExplorer/Request';
-import type {WrapperProps} from '@docusaurus/types';
+/**
+ * This is a swizzled wrapper for the Request component.
+ *
+ * In this case, we want to hide the Request component, so we return an empty
+ * JSX element.
+ */
+import React from "react";
+import type RequestType from "@theme/ApiExplorer/Request";
+import type { WrapperProps } from "@docusaurus/types";
 
 type Props = WrapperProps<typeof RequestType>;
 
 export default function RequestWrapper(props: Props): JSX.Element {
-  return (
-    <>
-      {/* <Request {...props} /> */}
-    </>
-  );
+  return <>{/* <Request {...props} /> */}</>;
 }
--- docs/src/theme/ApiExplorer/Response/index.tsx
+++ docs/src/theme/ApiExplorer/Response/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Response from '@theme-original/ApiExplorer/Response';
-import type ResponseType from '@theme/ApiExplorer/Response';
-import type {WrapperProps} from '@docusaurus/types';
+/**
+ * This is a swizzled wrapper for the Response component.
+ *
+ * In this case, we want to hide the Response component, so we return an empty
+ * JSX element.
+ */
+
+import React from "react";
+import type ResponseType from "@theme/ApiExplorer/Response";
+import type { WrapperProps } from "@docusaurus/types";
 
 type Props = WrapperProps<typeof ResponseType>;
 
 export default function ResponseWrapper(props: Props): JSX.Element {
-  return (
-    <>
-      {/* <Response {...props} /> */}
-    </>
-  );
+  return <></>;
 }
